bafcf16176aca964500d11bb7694f7ce4e885c50
sai-2116 <saikrishna.p@darwinbox.io>
2019-06-06 13:08:40 +0530

    new branch

--- src/app/components/basic-line/basic-line.component.ts
+++ src/app/components/basic-line/basic-line.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as d3 from 'd3';
+import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-basic-line',
@@ -10,6 +11,11 @@ export class BasicLineComponent implements OnInit {
 
   @ViewChild('chart')
   private chartContainer: ElementRef;
+
+  height = 340;
+  n = 20;
+
+  margin = {top: 50, right: 50, bottom: 50, left: 50};
 
   constructor() { }
 
@@ -22,11 +28,51 @@ export class BasicLineComponent implements OnInit {
 
     const element = this.chartContainer.nativeElement;
 
-    const svg = d3.select(element).append('svg')
-      .attr('width', element.offsetWidth)
-      .attr('height', 340);
+    const width = element.offsetWidth;
 
-      console.log('hello world');
+    const n = 21;
+
+    const xScale = d3.scaleLinear()
+    .domain([0, n - 1])
+    .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([this.height, 0]);
+
+    const dataset = d3.range(n).map(function(d) { return {'y': d3.randomUniform(1)() }; });
+
+    const line = d3.line()
+    .x(function(d, i) { return xScale(i); })
+    .y(function(d) { return yScale(d[1]);
+  })
+    .curve(d3.curveMonotoneX);
+
+    console.log('line', line);
+
+    const svg = d3.select(element).append('svg')
+    .attr('width', width + this.margin.left + this.margin.right)
+    .attr('height', this.height + this.margin.top + this.margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+    svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + this.height + ')')
+    .call(d3.axisBottom(xScale));
+
+    svg.append('g')
+    .attr('class', 'y axis')
+    .call(d3.axisLeft(yScale));
+
+    // svg.append('path')
+    // .datum(dataset)
+    // .attr('class', 'line')
+    // .attr('d', line.d);
+
   }
+  // onResize() {
+  //   this.createChart();
+  // }
 
 }
